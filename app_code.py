691b100191ade17c2c303d2c00916f1ffe8396ed
girtsfr <87335220+girtsfr@users.noreply.github.com>
2023-01-11 23:20:35 +0200

    Update app_code.py

--- app_code.py
+++ app_code.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 data = pd.read_csv('flats_for_rent_2023-01-05.csv')
 data = data[['rooms', 'square_m', 'floor', 'price', 'area', 'region', 'price_per_square_m']]
@@ -12,9 +12,17 @@ st.text('web app text')
 st.text_input('first name')
 st.number_input('pick a number')
 
-fig, ax = plt.subplots()
-ax.hist(data['price_per_square_m'], bins=100)
-st.pyplot(fig)
+import plotly.express as px
+
+df = px.data.tips()
+fig = px.histogram(data, x="price_per_square_m", nbins=100)
+# fig.show()
+st.plotly_chart(fig)
+
+
+# fig, ax = plt.subplots()
+# ax.hist(data['price_per_square_m'], bins=100)
+# st.pyplot(fig)
 
 # ct = pd.cut(data['price_per_square_m'], 100).value_counts(sort=False)
 # ct = pd.DataFrame(ct)
