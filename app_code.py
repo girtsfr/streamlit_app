3d04ba2d4491ec7fa1611bee1f6f350fd1b3e837
girtsfr <87335220+girtsfr@users.noreply.github.com>
2023-01-11 23:39:33 +0200

    Update app_code.py

--- app_code.py
+++ app_code.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-# import matplotlib.pyplot as plt
 
 data = pd.read_csv('flats_for_rent_2023-01-05.csv')
 data = data[['rooms', 'square_m', 'floor', 'price', 'area', 'region', 'price_per_square_m']]
@@ -18,14 +17,4 @@ df = px.data.tips()
 fig = px.histogram(data, x="price_per_square_m", nbins=100)
 st.plotly_chart(fig, theme="streamlit")
 
-
-# fig, ax = plt.subplots()
-# ax.hist(data['price_per_square_m'], bins=100)
-# st.pyplot(fig)
-
-# ct = pd.cut(data['price_per_square_m'], 100).value_counts(sort=False)
-# ct = pd.DataFrame(ct)
-
-# st.bar_chart(ct)
-
-# st.dataframe(data)
+st.dataframe(data)
