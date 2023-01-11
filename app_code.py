0b94ff27b31d93150ae989e303d36717f76f0aeb
girtsfr <87335220+girtsfr@users.noreply.github.com>
2023-01-11 23:02:16 +0200

    Update app_code.py

--- app_code.py
+++ app_code.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('flats_for_rent_2023-01-05.csv')
 data = data[['rooms', 'square_m', 'floor', 'price', 'area', 'region', 'price_per_square_m']]
@@ -12,13 +12,13 @@ st.text('web app text')
 st.text_input('first name')
 st.number_input('pick a number')
 
-# fig, ax = plt.subplots()
-# ax.hist(data['price_per_square_m'], bins=100)
-# st.pyplot(fig)
+fig, ax = plt.subplots()
+ax.hist(data['price_per_square_m'], bins=100)
+st.pyplot(fig)
 
-ct = pd.cut(data['price_per_square_m'], 100).value_counts(sort=False)
-ct = pd.DataFrame(ct)
+# ct = pd.cut(data['price_per_square_m'], 100).value_counts(sort=False)
+# ct = pd.DataFrame(ct)
 
-st.bar_chart(ct)
+# st.bar_chart(ct)
 
 # st.dataframe(data)
