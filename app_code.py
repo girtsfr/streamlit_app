d96d53b04a1b215a7819f176affe0db0b70822f3
girtsfr <87335220+girtsfr@users.noreply.github.com>
2023-01-12 18:07:42 +0200

    Update app_code.py

--- app_code.py
+++ app_code.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 data = pd.read_csv('flats_for_rent.csv')
-data = data[['rooms', 'square_m', 'floor', 'price', 'area', 'region', 'price_per_square_m']]
+data = data[['rooms', 'square_m', 'floor', 'price', 'area', 'region', 'price_per_square_m', 'time']]
 data = data[data['price_per_square_m'] < 50]
 data = data[data['area'] == 'riga']
 
