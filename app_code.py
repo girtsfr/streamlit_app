37a432e7516b9c6fdc5fcdf335f22c48afda0199
girtsfr <87335220+girtsfr@users.noreply.github.com>
2023-01-13 17:47:50 +0200

    Update app_code.py

--- app_code.py
+++ app_code.py
@@ -1,19 +1,22 @@
 import streamlit as st
 import pandas as pd
 
-data = pd.read_csv('flats_for_rent.csv')
-data = data[['rooms', 'square_m', 'floor', 'price', 'area', 'region', 'price_per_square_m', 'time']]
-data = data[data['price_per_square_m'] < 50]
-data = data[data['area'] == 'riga']
+for_sale_data = pd.read_csv('flats_for_sale.csv')
+for_sale_data['time'] = pd.to_datetime(for_sale_data['time'], format='%Y-%m-%d')
 
-st.title('web app title')
-st.text('web app text')
-st.text_input('first name')
-st.number_input('pick a number')
+st.dataframe(for_sale_data)
 
-import plotly.express as px
 
-fig = px.histogram(data, x="price_per_square_m", nbins=100)
-st.plotly_chart(fig, theme="streamlit")
 
-st.dataframe(data)
+# st.title('web app title')
+# st.text('web app text')
+# st.text_input('first name')
+# st.number_input('pick a number')
+
+# import plotly.express as px
+
+# fig = px.histogram(data, x="price_per_square_m", nbins=100)
+# st.plotly_chart(fig, theme="streamlit")
+
+# st.dataframe(data)
+
