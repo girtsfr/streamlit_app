b2326b60ee2576da45e5f1375de86ca7f1f63d5c
girtsfr <87335220+girtsfr@users.noreply.github.com>
2023-12-03 15:54:14 +0200

    Update app_code.py

--- app_code.py
+++ app_code.py
@@ -24,240 +24,224 @@ numeric_cols = ['rooms', 'square_m', 'floor', 'price', 'building_total_floors', 
 rent_data[numeric_cols] = rent_data[numeric_cols].astype(np.int64)
 
 
-######################################################################
-### DEFINE FILTER RANGES
-# max_floors = int(sale_data['floor'].max())
-# max_rooms = int(sale_data['rooms'].max())
-# max_size = int(sale_data['square_m'].max())
-max_floors = int(33)
-max_rooms = int(6)
-max_size = int(640)
+# ######################################################################
+# ### DEFINE FILTER RANGES
+# # max_floors = int(sale_data['floor'].max())
+# # max_rooms = int(sale_data['rooms'].max())
+# # max_size = int(sale_data['square_m'].max())
+# max_floors = int(33)
+# max_rooms = int(6)
+# max_size = int(640)
 
 
-######################################################################
-### SIDEBAR ###
-### REGION SELECTION
-regions = sale_data['region'].value_counts().index.values
-regions = np.insert(regions, 0, 'All regions')
+# ######################################################################
+# ### SIDEBAR ###
+# ### REGION SELECTION
+# regions = sale_data['region'].value_counts().index.values
+# regions = np.insert(regions, 0, 'All regions')
 
-select_region = st.sidebar.selectbox('Select region:', regions)
+# select_region = st.sidebar.selectbox('Select region:', regions)
 
-if select_region != 'All regions':
-    sale_data = sale_data[sale_data['region'] == select_region]
-    rent_data = rent_data[rent_data['region'] == select_region]
+# if select_region != 'All regions':
+#     sale_data = sale_data[sale_data['region'] == select_region]
+#     rent_data = rent_data[rent_data['region'] == select_region]
     
     
-### FLOOR SELECTION
-select_floor = st.sidebar.slider('Select floor:', value=[1, max_floors], min_value = 1, max_value = max_floors)
+# ### FLOOR SELECTION
+# select_floor = st.sidebar.slider('Select floor:', value=[1, max_floors], min_value = 1, max_value = max_floors)
 
-sale_data = sale_data[sale_data['floor'] >= select_floor[0]]
-sale_data = sale_data[sale_data['floor'] <= select_floor[1]]
+# sale_data = sale_data[sale_data['floor'] >= select_floor[0]]
+# sale_data = sale_data[sale_data['floor'] <= select_floor[1]]
 
-rent_data = rent_data[rent_data['floor'] >= select_floor[0]]
-rent_data = rent_data[rent_data['floor'] <= select_floor[1]]
-
-
-### ROOM COUNT SELECTION
-select_rooms = st.sidebar.slider('Select room count:', value=[1, max_rooms], min_value = 1, max_value = max_rooms)
-
-sale_data = sale_data[sale_data['rooms'] >= select_rooms[0]]
-sale_data = sale_data[sale_data['rooms'] <= select_rooms[1]]
-
-rent_data = rent_data[rent_data['rooms'] >= select_rooms[0]]
-rent_data = rent_data[rent_data['rooms'] <= select_rooms[1]]
+# rent_data = rent_data[rent_data['floor'] >= select_floor[0]]
+# rent_data = rent_data[rent_data['floor'] <= select_floor[1]]
 
 
-### SIZE SELECTION
-select_size = st.sidebar.slider('Select size (square meters):', value=[1, max_size], min_value = 1, max_value = max_size)
+# ### ROOM COUNT SELECTION
+# select_rooms = st.sidebar.slider('Select room count:', value=[1, max_rooms], min_value = 1, max_value = max_rooms)
 
-sale_data = sale_data[sale_data['square_m'] >= select_size[0]]
-sale_data = sale_data[sale_data['square_m'] <= select_size[1]]
+# sale_data = sale_data[sale_data['rooms'] >= select_rooms[0]]
+# sale_data = sale_data[sale_data['rooms'] <= select_rooms[1]]
 
-rent_data = rent_data[rent_data['square_m'] >= select_size[0]]
-rent_data = rent_data[rent_data['square_m'] <= select_size[1]]
-
-
-######################################################################
-### CREATE SUMMARY TABLE
-sale_summary = sale_data.groupby('time')
-sale_summary = sale_summary.agg(
-                                count = ('price', 'count'),
-                                mean_price_per_square = ('price_per_square_m', 'mean'),
-                                median_price_per_square = ('price_per_square_m', 'median'),
-                                p_sum = ('price', 'sum'),
-                                mean_rooms = ('rooms', 'mean'),
-                                mean_square_m = ('square_m', 'mean')
-                                )
-
-rent_summary = rent_data.groupby('time')
-rent_summary = rent_summary.agg(
-                                count = ('price', 'count'),
-                                mean_price_per_square = ('price_per_square_m', 'mean'),
-                                median_price_per_square = ('price_per_square_m', 'median'),
-                                p_sum = ('price', 'sum'),
-                                mean_rooms = ('rooms', 'mean'),
-                                mean_square_m = ('square_m', 'mean')
-                                )
+# rent_data = rent_data[rent_data['rooms'] >= select_rooms[0]]
+# rent_data = rent_data[rent_data['rooms'] <= select_rooms[1]]
 
 
-######################################################################
-### TABS ###
-# sale_tab, rent_tab = st.tabs(['FOR SALE', 'FOR RENT'])
-sale_tab, rent_tab, yields_tab = st.tabs(['FOR SALE', 'FOR RENT', 'YIELDS'])
+# ### SIZE SELECTION
+# select_size = st.sidebar.slider('Select size (square meters):', value=[1, max_size], min_value = 1, max_value = max_size)
 
-######################################################################
-### FOR SALE TAB ###
-sale_tab.header('Apartments for sale')
-sale_tab.caption('On the sidebar at the left, you can specify the criteria by which you want to filter the data. All charts and tables in all the tabs are automatically updated right after the filter setting is changed. Once per day, at around midnight, the dataset is updated with the latest advertisement data.')
-sale_tab.caption('')
+# sale_data = sale_data[sale_data['square_m'] >= select_size[0]]
+# sale_data = sale_data[sale_data['square_m'] <= select_size[1]]
 
-### CHARTS
-sale_tab.subheader('Count of listings')
-sale_tab.caption('Below chart shows how many apartments were listed for sale at particular dates')
-fig_sale_count = px.line(sale_summary, y='count')
-sale_tab.plotly_chart(fig_sale_count, theme="streamlit")
-
-sale_tab.subheader('Average price per square meter')
-sale_tab.caption('Below chart shows what was the average price per square meter at particular dates')
-fig_sale_price = px.line(sale_summary, y='mean_price_per_square', labels={'mean_price_per_square':'mean price per square meter'})
-sale_tab.plotly_chart(fig_sale_price, theme="streamlit")
+# rent_data = rent_data[rent_data['square_m'] >= select_size[0]]
+# rent_data = rent_data[rent_data['square_m'] <= select_size[1]]
 
 
-### FILTER PAST X MONTHS
+# ######################################################################
+# ### CREATE SUMMARY TABLE
+# sale_summary = sale_data.groupby('time')
+# sale_summary = sale_summary.agg(
+#                                 count = ('price', 'count'),
+#                                 mean_price_per_square = ('price_per_square_m', 'mean'),
+#                                 median_price_per_square = ('price_per_square_m', 'median'),
+#                                 p_sum = ('price', 'sum'),
+#                                 mean_rooms = ('rooms', 'mean'),
+#                                 mean_square_m = ('square_m', 'mean')
+#                                 )
 
-sale_tab.subheader('Price distribution for listings posted in the last X months')
-sale_tab.caption('Below histogram shows the distribution of prices per square meter for advertisments that were posted in the last X months (you can specify the lookback period)')
-
-sale_months_back = sale_tab.number_input('Choose lookback time frame (in months):', min_value=1, max_value=12, value=6, key='sales_lookback')
-sale_hist = sale_data[sale_data['time'] >= pd.to_datetime('now') - pd.DateOffset(months=sale_months_back)]
-
-sale_hist['idx'] = (
-    sale_hist['rooms'].astype(str)
-    + '-'
-    + sale_hist['square_m'].astype(str)
-    + '-'
-    + sale_hist['floor'].astype(str)
-    + '-'
-    + sale_hist['price'].astype(str)
-    + '-'
-    + sale_hist['street'].astype(str)
-)
-
-sale_hist = sale_hist.drop_duplicates(subset='idx')
-
-fig_sale_histogram = px.histogram(sale_hist, x="price_per_square_m")
-fig_sale_histogram.update_xaxes(range=[0, 5000])
-
-sale_tab.plotly_chart(fig_sale_histogram, theme="streamlit")
+# rent_summary = rent_data.groupby('time')
+# rent_summary = rent_summary.agg(
+#                                 count = ('price', 'count'),
+#                                 mean_price_per_square = ('price_per_square_m', 'mean'),
+#                                 median_price_per_square = ('price_per_square_m', 'median'),
+#                                 p_sum = ('price', 'sum'),
+#                                 mean_rooms = ('rooms', 'mean'),
+#                                 mean_square_m = ('square_m', 'mean')
+#                                 )
 
 
-### DATAFRAME WITH CURRENT OPEN LISTINGS
-sale_open_listings = sale_data[~sale_data['link'].isna()][[
-    'street',
-    'square_m',
-    'rooms',
-    'floor',
-#     'building_total_floors',
-#     'serie',
-    'price',
-    'price_per_square_m',
-    'link']]
+# ######################################################################
+# ### TABS ###
+# # sale_tab, rent_tab = st.tabs(['FOR SALE', 'FOR RENT'])
+# sale_tab, rent_tab, yields_tab = st.tabs(['FOR SALE', 'FOR RENT', 'YIELDS'])
 
-sale_tab.subheader('Active listings')
-sale_tab.caption('Below table shows all apartment listings that are currently active')
-sale_tab.dataframe(sale_open_listings)
+# ######################################################################
+# ### FOR SALE TAB ###
+# sale_tab.header('Apartments for sale')
+# sale_tab.caption('On the sidebar at the left, you can specify the criteria by which you want to filter the data. All charts and tables in all the tabs are automatically updated right after the filter setting is changed. Once per day, at around midnight, the dataset is updated with the latest advertisement data.')
+# sale_tab.caption('')
 
+# ### CHARTS
+# sale_tab.subheader('Count of listings')
+# sale_tab.caption('Below chart shows how many apartments were listed for sale at particular dates')
+# fig_sale_count = px.line(sale_summary, y='count')
+# sale_tab.plotly_chart(fig_sale_count, theme="streamlit")
 
-
-######################################################################
-### FOR RENT TAB ###
-rent_tab.header('Apartments for rent')
-rent_tab.caption('On the sidebar at the left, you can specify the criteria by which you want to filter the data. All charts and tables in all the tabs are automatically updated right after the filter setting is changed. Once per day, at around midnight, the dataset is updated with the latest advertisement data.')
-rent_tab.caption('')
-
-### CHARTS
-rent_tab.subheader('Count of listings')
-rent_tab.caption('Below chart shows how many apartments were listed for rent at particular dates')
-fig_rent_count = px.line(rent_summary, y='count')
-rent_tab.plotly_chart(fig_rent_count, theme="streamlit")
-
-rent_tab.subheader('Average price per square meter')
-rent_tab.caption('Below chart shows what was the average price per square meter at particular dates')
-fig_rent_price = px.line(rent_summary, y='mean_price_per_square', labels={'mean_price_per_square':'mean price per square meter'})
-rent_tab.plotly_chart(fig_rent_price, theme="streamlit")
+# sale_tab.subheader('Average price per square meter')
+# sale_tab.caption('Below chart shows what was the average price per square meter at particular dates')
+# fig_sale_price = px.line(sale_summary, y='mean_price_per_square', labels={'mean_price_per_square':'mean price per square meter'})
+# sale_tab.plotly_chart(fig_sale_price, theme="streamlit")
 
 
-### FILTER PAST X MONTHS
+# ### FILTER PAST X MONTHS
 
-rent_tab.subheader('Price distribution for listings posted in the last X months')
-rent_tab.caption('Below histogram shows the distribution of prices per square meter for advertisments that were posted in the last X months (you can specify the lookback period)')
+# sale_tab.subheader('Price distribution for listings posted in the last X months')
+# sale_tab.caption('Below histogram shows the distribution of prices per square meter for advertisments that were posted in the last X months (you can specify the lookback period)')
 
-rent_months_back = rent_tab.number_input('Choose lookback time frame (in months):', min_value=1, max_value=12, value=6, key='rents_lookback')
-rent_hist = rent_data[rent_data['time'] >= pd.to_datetime('now') - pd.DateOffset(months=rent_months_back)]
+# sale_months_back = sale_tab.number_input('Choose lookback time frame (in months):', min_value=1, max_value=12, value=6, key='sales_lookback')
+# sale_hist = sale_data[sale_data['time'] >= pd.to_datetime('now') - pd.DateOffset(months=sale_months_back)]
 
-rent_hist['idx'] = (
-    rent_hist['rooms'].astype(str)
-    + '-'
-    + rent_hist['square_m'].astype(str)
-    + '-'
-    + rent_hist['floor'].astype(str)
-    + '-'
-    + rent_hist['price'].astype(str)
-    + '-'
-    + rent_hist['street'].astype(str)
-)
+# sale_hist['idx'] = (
+#     sale_hist['rooms'].astype(str)
+#     + '-'
+#     + sale_hist['square_m'].astype(str)
+#     + '-'
+#     + sale_hist['floor'].astype(str)
+#     + '-'
+#     + sale_hist['price'].astype(str)
+#     + '-'
+#     + sale_hist['street'].astype(str)
+# )
 
-rent_hist = rent_hist.drop_duplicates(subset='idx')
+# sale_hist = sale_hist.drop_duplicates(subset='idx')
 
-fig_rent_histogram = px.histogram(rent_hist, x="price_per_square_m")
-fig_rent_histogram.update_xaxes(range=[0, 30])
+# fig_sale_histogram = px.histogram(sale_hist, x="price_per_square_m")
+# fig_sale_histogram.update_xaxes(range=[0, 5000])
 
-rent_tab.plotly_chart(fig_rent_histogram, theme="streamlit")
-
-
-### DATAFRAME WITH CURRENT OPEN LISTINGS
-rent_open_listings = rent_data[~rent_data['link'].isna()][[
-    'street',
-    'square_m',
-    'rooms',
-    'floor',
-#     'building_total_floors',
-#     'serie',
-    'price',
-    'price_per_square_m',
-    'link']]
+# sale_tab.plotly_chart(fig_sale_histogram, theme="streamlit")
 
 
-rent_tab.subheader('Active listings')
-rent_tab.caption('Below table shows all apartment listings that are currently active')
-rent_tab.dataframe(rent_open_listings)
+# ### DATAFRAME WITH CURRENT OPEN LISTINGS
+# sale_open_listings = sale_data[~sale_data['link'].isna()][[
+#     'street',
+#     'square_m',
+#     'rooms',
+#     'floor',
+# #     'building_total_floors',
+# #     'serie',
+#     'price',
+#     'price_per_square_m',
+#     'link']]
+
+# sale_tab.subheader('Active listings')
+# sale_tab.caption('Below table shows all apartment listings that are currently active')
+# sale_tab.dataframe(sale_open_listings)
 
 
-######################################################################
-### YIELDS TAB ###
 
-yield_annual = ((rent_summary['mean_price_per_square'] * 12) / sale_summary['mean_price_per_square']) * 100
+# ######################################################################
+# ### FOR RENT TAB ###
+# rent_tab.header('Apartments for rent')
+# rent_tab.caption('On the sidebar at the left, you can specify the criteria by which you want to filter the data. All charts and tables in all the tabs are automatically updated right after the filter setting is changed. Once per day, at around midnight, the dataset is updated with the latest advertisement data.')
+# rent_tab.caption('')
 
-### CHARTS
-yields_tab.subheader('Annual yield')
-yields_tab.caption('Below chart shows the annual yield of renting out an apartment, according to average rent and sale price per square meter. The formula is:')
-yields_tab.caption('(AVG rent price per square meter * 12)  /  AVG sale price per square meter')
+# ### CHARTS
+# rent_tab.subheader('Count of listings')
+# rent_tab.caption('Below chart shows how many apartments were listed for rent at particular dates')
+# fig_rent_count = px.line(rent_summary, y='count')
+# rent_tab.plotly_chart(fig_rent_count, theme="streamlit")
 
-fig_yield = px.line(yield_annual, y='mean_price_per_square', labels={'mean_price_per_square':'annual yield (%)'})
-yields_tab.plotly_chart(fig_yield, theme="streamlit")
+# rent_tab.subheader('Average price per square meter')
+# rent_tab.caption('Below chart shows what was the average price per square meter at particular dates')
+# fig_rent_price = px.line(rent_summary, y='mean_price_per_square', labels={'mean_price_per_square':'mean price per square meter'})
+# rent_tab.plotly_chart(fig_rent_price, theme="streamlit")
 
 
-### BOKEH ###
-# from bokeh.plotting import figure
-# from bokeh.io import curdoc
-# curdoc().theme = 'dark_minimal'
+# ### FILTER PAST X MONTHS
 
-# summary_chart = figure(x_axis_type="datetime", plot_height=500)
-# summary_chart.line(sale_summary.index, sale_summary['count'])
-# curdoc().add_root(summary_chart)
-# st.bokeh_chart(summary_chart, use_container_width=True)
+# rent_tab.subheader('Price distribution for listings posted in the last X months')
+# rent_tab.caption('Below histogram shows the distribution of prices per square meter for advertisments that were posted in the last X months (you can specify the lookback period)')
 
-# st.title('web app title')
-# st.text('web app text')
-# st.text_input('first name')
+# rent_months_back = rent_tab.number_input('Choose lookback time frame (in months):', min_value=1, max_value=12, value=6, key='rents_lookback')
+# rent_hist = rent_data[rent_data['time'] >= pd.to_datetime('now') - pd.DateOffset(months=rent_months_back)]
 
+# rent_hist['idx'] = (
+#     rent_hist['rooms'].astype(str)
+#     + '-'
+#     + rent_hist['square_m'].astype(str)
+#     + '-'
+#     + rent_hist['floor'].astype(str)
+#     + '-'
+#     + rent_hist['price'].astype(str)
+#     + '-'
+#     + rent_hist['street'].astype(str)
+# )
+
+# rent_hist = rent_hist.drop_duplicates(subset='idx')
+
+# fig_rent_histogram = px.histogram(rent_hist, x="price_per_square_m")
+# fig_rent_histogram.update_xaxes(range=[0, 30])
+
+# rent_tab.plotly_chart(fig_rent_histogram, theme="streamlit")
+
+
+# ### DATAFRAME WITH CURRENT OPEN LISTINGS
+# rent_open_listings = rent_data[~rent_data['link'].isna()][[
+#     'street',
+#     'square_m',
+#     'rooms',
+#     'floor',
+# #     'building_total_floors',
+# #     'serie',
+#     'price',
+#     'price_per_square_m',
+#     'link']]
+
+
+# rent_tab.subheader('Active listings')
+# rent_tab.caption('Below table shows all apartment listings that are currently active')
+# rent_tab.dataframe(rent_open_listings)
+
+
+# ######################################################################
+# ### YIELDS TAB ###
+
+# yield_annual = ((rent_summary['mean_price_per_square'] * 12) / sale_summary['mean_price_per_square']) * 100
+
+# ### CHARTS
+# yields_tab.subheader('Annual yield')
+# yields_tab.caption('Below chart shows the annual yield of renting out an apartment, according to average rent and sale price per square meter. The formula is:')
+# yields_tab.caption('(AVG rent price per square meter * 12)  /  AVG sale price per square meter')
+
+# fig_yield = px.line(yield_annual, y='mean_price_per_square', labels={'mean_price_per_square':'annual yield (%)'})
+# yields_tab.plotly_chart(fig_yield, theme="streamlit")
