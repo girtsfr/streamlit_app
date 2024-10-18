36351ace94d9a61705a7690d16a4f13875fbe23e
girtsfr <87335220+girtsfr@users.noreply.github.com>
2024-10-18 21:29:31 +0300

    Update app_code.py

--- app_code.py
+++ app_code.py
@@ -5,122 +5,107 @@ import plotly.express as px
 
 ######################################################################
 ### IMPORTING DATA ###
-### FOR SALE DATA
-sale_data = pd.read_csv('flats_for_sale.csv')
-
-sale_data['time'] = pd.to_datetime(sale_data['time'], format='%Y-%m-%d')
-sale_data = sale_data.dropna(subset=['rooms'])
-
-numeric_cols = ['rooms', 'square_m', 'floor', 'price', 'building_total_floors', 'price_per_square_m']
-sale_data[numeric_cols] = sale_data[numeric_cols].astype(np.int64)
-
-### FOR RENT DATA
-rent_data = pd.read_csv('flats_for_rent.csv')
-
-rent_data['time'] = pd.to_datetime(rent_data['time'], format='%Y-%m-%d')
-rent_data = rent_data.dropna(subset=['rooms'])
-
-numeric_cols = ['rooms', 'square_m', 'floor', 'price', 'building_total_floors', 'price_per_square_m']
-rent_data[numeric_cols] = rent_data[numeric_cols].astype(np.int64)
+sale_data = pd.read_pickle('sale_data.pkl')
+rent_data = pd.read_pickle('rent_data.pkl')
 
 
-# ######################################################################
-# ### DEFINE FILTER RANGES
-# # max_floors = int(sale_data['floor'].max())
-# # max_rooms = int(sale_data['rooms'].max())
-# # max_size = int(sale_data['square_m'].max())
-# max_floors = int(33)
-# max_rooms = int(6)
-# max_size = int(640)
+######################################################################
+### DEFINE FILTER RANGES
+max_floors = int(sale_data['floor'].max())
+max_rooms = int(sale_data['rooms'].max())
+max_size = int(sale_data['square_m'].max())
+max_floors = int(33)
+max_rooms = int(6)
+max_size = int(640)
 
 
-# ######################################################################
-# ### SIDEBAR ###
-# ### REGION SELECTION
-# regions = sale_data['region'].value_counts().index.values
-# regions = np.insert(regions, 0, 'All regions')
+######################################################################
+### SIDEBAR ###
+### REGION SELECTION
+regions = sale_data['region'].value_counts().index.values
+regions = np.insert(regions, 0, 'All regions')
 
-# select_region = st.sidebar.selectbox('Select region:', regions)
+select_region = st.sidebar.selectbox('Select region:', regions)
 
-# if select_region != 'All regions':
-#     sale_data = sale_data[sale_data['region'] == select_region]
-#     rent_data = rent_data[rent_data['region'] == select_region]
+if select_region != 'All regions':
+    sale_data = sale_data[sale_data['region'] == select_region]
+    rent_data = rent_data[rent_data['region'] == select_region]
     
     
-# ### FLOOR SELECTION
-# select_floor = st.sidebar.slider('Select floor:', value=[1, max_floors], min_value = 1, max_value = max_floors)
+### FLOOR SELECTION
+select_floor = st.sidebar.slider('Select floor:', value=[1, max_floors], min_value = 1, max_value = max_floors)
 
-# sale_data = sale_data[sale_data['floor'] >= select_floor[0]]
-# sale_data = sale_data[sale_data['floor'] <= select_floor[1]]
+sale_data = sale_data[sale_data['floor'] >= select_floor[0]]
+sale_data = sale_data[sale_data['floor'] <= select_floor[1]]
 
-# rent_data = rent_data[rent_data['floor'] >= select_floor[0]]
-# rent_data = rent_data[rent_data['floor'] <= select_floor[1]]
-
-
-# ### ROOM COUNT SELECTION
-# select_rooms = st.sidebar.slider('Select room count:', value=[1, max_rooms], min_value = 1, max_value = max_rooms)
-
-# sale_data = sale_data[sale_data['rooms'] >= select_rooms[0]]
-# sale_data = sale_data[sale_data['rooms'] <= select_rooms[1]]
-
-# rent_data = rent_data[rent_data['rooms'] >= select_rooms[0]]
-# rent_data = rent_data[rent_data['rooms'] <= select_rooms[1]]
+rent_data = rent_data[rent_data['floor'] >= select_floor[0]]
+rent_data = rent_data[rent_data['floor'] <= select_floor[1]]
 
 
-# ### SIZE SELECTION
-# select_size = st.sidebar.slider('Select size (square meters):', value=[1, max_size], min_value = 1, max_value = max_size)
+### ROOM COUNT SELECTION
+select_rooms = st.sidebar.slider('Select room count:', value=[1, max_rooms], min_value = 1, max_value = max_rooms)
 
-# sale_data = sale_data[sale_data['square_m'] >= select_size[0]]
-# sale_data = sale_data[sale_data['square_m'] <= select_size[1]]
+sale_data = sale_data[sale_data['rooms'] >= select_rooms[0]]
+sale_data = sale_data[sale_data['rooms'] <= select_rooms[1]]
 
-# rent_data = rent_data[rent_data['square_m'] >= select_size[0]]
-# rent_data = rent_data[rent_data['square_m'] <= select_size[1]]
-
-
-# ######################################################################
-# ### CREATE SUMMARY TABLE
-# sale_summary = sale_data.groupby('time')
-# sale_summary = sale_summary.agg(
-#                                 count = ('price', 'count'),
-#                                 mean_price_per_square = ('price_per_square_m', 'mean'),
-#                                 median_price_per_square = ('price_per_square_m', 'median'),
-#                                 p_sum = ('price', 'sum'),
-#                                 mean_rooms = ('rooms', 'mean'),
-#                                 mean_square_m = ('square_m', 'mean')
-#                                 )
-
-# rent_summary = rent_data.groupby('time')
-# rent_summary = rent_summary.agg(
-#                                 count = ('price', 'count'),
-#                                 mean_price_per_square = ('price_per_square_m', 'mean'),
-#                                 median_price_per_square = ('price_per_square_m', 'median'),
-#                                 p_sum = ('price', 'sum'),
-#                                 mean_rooms = ('rooms', 'mean'),
-#                                 mean_square_m = ('square_m', 'mean')
-#                                 )
+rent_data = rent_data[rent_data['rooms'] >= select_rooms[0]]
+rent_data = rent_data[rent_data['rooms'] <= select_rooms[1]]
 
 
-# ######################################################################
-# ### TABS ###
-# # sale_tab, rent_tab = st.tabs(['FOR SALE', 'FOR RENT'])
-# sale_tab, rent_tab, yields_tab = st.tabs(['FOR SALE', 'FOR RENT', 'YIELDS'])
+### SIZE SELECTION
+select_size = st.sidebar.slider('Select size (square meters):', value=[1, max_size], min_value = 1, max_value = max_size)
 
-# ######################################################################
-# ### FOR SALE TAB ###
-# sale_tab.header('Apartments for sale')
-# sale_tab.caption('On the sidebar at the left, you can specify the criteria by which you want to filter the data. All charts and tables in all the tabs are automatically updated right after the filter setting is changed. Once per day, at around midnight, the dataset is updated with the latest advertisement data.')
-# sale_tab.caption('')
+sale_data = sale_data[sale_data['square_m'] >= select_size[0]]
+sale_data = sale_data[sale_data['square_m'] <= select_size[1]]
 
-# ### CHARTS
-# sale_tab.subheader('Count of listings')
-# sale_tab.caption('Below chart shows how many apartments were listed for sale at particular dates')
-# fig_sale_count = px.line(sale_summary, y='count')
-# sale_tab.plotly_chart(fig_sale_count, theme="streamlit")
+rent_data = rent_data[rent_data['square_m'] >= select_size[0]]
+rent_data = rent_data[rent_data['square_m'] <= select_size[1]]
 
-# sale_tab.subheader('Average price per square meter')
-# sale_tab.caption('Below chart shows what was the average price per square meter at particular dates')
-# fig_sale_price = px.line(sale_summary, y='mean_price_per_square', labels={'mean_price_per_square':'mean price per square meter'})
-# sale_tab.plotly_chart(fig_sale_price, theme="streamlit")
+
+######################################################################
+### CREATE SUMMARY TABLE
+sale_summary = sale_data.groupby('time')
+sale_summary = sale_summary.agg(
+                                count = ('price', 'count'),
+                                mean_price_per_square = ('price_per_square_m', 'mean'),
+                                median_price_per_square = ('price_per_square_m', 'median'),
+                                p_sum = ('price', 'sum'),
+                                mean_rooms = ('rooms', 'mean'),
+                                mean_square_m = ('square_m', 'mean')
+                                )
+
+rent_summary = rent_data.groupby('time')
+rent_summary = rent_summary.agg(
+                                count = ('price', 'count'),
+                                mean_price_per_square = ('price_per_square_m', 'mean'),
+                                median_price_per_square = ('price_per_square_m', 'median'),
+                                p_sum = ('price', 'sum'),
+                                mean_rooms = ('rooms', 'mean'),
+                                mean_square_m = ('square_m', 'mean')
+                                )
+
+
+######################################################################
+### TABS ###
+# sale_tab, rent_tab = st.tabs(['FOR SALE', 'FOR RENT'])
+sale_tab, rent_tab, yields_tab = st.tabs(['FOR SALE', 'FOR RENT', 'YIELDS'])
+
+######################################################################
+### FOR SALE TAB ###
+sale_tab.header('Apartments for sale')
+sale_tab.caption('On the sidebar at the left, you can specify the criteria by which you want to filter the data. All charts and tables in all the tabs are automatically updated right after the filter setting is changed. Once per day, at around midnight, the dataset is updated with the latest advertisement data.')
+sale_tab.caption('')
+
+### CHARTS
+sale_tab.subheader('Count of listings')
+sale_tab.caption('Below chart shows how many apartments were listed for sale at particular dates')
+fig_sale_count = px.line(sale_summary, y='count')
+sale_tab.plotly_chart(fig_sale_count, theme="streamlit")
+
+sale_tab.subheader('Average price per square meter')
+sale_tab.caption('Below chart shows what was the average price per square meter at particular dates')
+fig_sale_price = px.line(sale_summary, y='mean_price_per_square', labels={'mean_price_per_square':'mean price per square meter'})
+sale_tab.plotly_chart(fig_sale_price, theme="streamlit")
 
 
 # ### FILTER PAST X MONTHS
